736b09745663acda77ab081eae48ddb2c3a5399f
Ari Hartikainen <ahartikainen@users.noreply.github.com>
2021-08-30 22:15:20 +0300

    Update compare_cmdstanpy.py

--- compare_cmdstanpy.py
+++ compare_cmdstanpy.py
@@ -5,10 +5,12 @@ import numpy as np
 
 np.set_printoptions(precision=16)
 
-files = glob("./*.txt")
+files = glob("./Upload stdout files/*.txt")
 
+print("\nStarting file processing")
 results = {}
-for path in files:
+i = -1
+for i, path in enumerate(files):
     with open(path) as f:
         res = pd.read_csv(f, float_precision="round_trip", index=None)
         _, fname = os.path.split(os.path.splitext(path)[0])
@@ -16,7 +18,7 @@ for path in files:
         if seed not in results:
             results[seed] = {}
         results[seed][platform] = res
-
+print(f"Processed total of {i + 1} files.")
 
 for seed, sub_dict in results.items():
     if (sub_dict["Windows"].iloc[:, 0] == sub_dict["Linux"].iloc[:, 0]).all():
@@ -30,3 +32,4 @@ for seed, sub_dict in results.items():
             ).idxmax()
             + 1,
         )
+print("\nScript finished")
